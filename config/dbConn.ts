0e202ca9891850fe33a1b1925adbd5eec2c617c0
Krrish <malekm09122003@gmail.com>
2023-04-26 01:05:08 +0530

    Initialised MongoDb connection functionality

---
+++ config/dbConn.ts
@@ -0,0 +1,13 @@
+import { connect } from 'mongoose';
+
+require('dotenv').config({path: '../.env'})
+
+const connectDB = async () => {
+    try{
+        await connect(process.env.DATABASE_URI);
+    } catch(err){
+        console.log(err)
+    }
+}
+
+export {connectDB};
